9e724ffdec3ae7b30df8a2bcbac91ab1741536ea
George Perdrizet <george@perdrizet.org>
2022-01-06 22:55:27 -0500

    Fixed issue with boolean command line parameters

--- scan.py
+++ scan.py
@@ -29,21 +29,25 @@ import config
 # Instantiate command line argument parser
 parser = argparse.ArgumentParser(description='Controls scanner and sesnors.')
 
-# Add arguments to specify whether or not to read sensor data
-parser.add_argument(
-    '--sensors', 
-    type=str, 
-    help='(True|False) Read sensor data.'
-)
+# Add argument to specify whether or not to read sensor data
+parser.add_argument('--sensors', dest='sensors', action='store_true')
+parser.add_argument('--no-sensors', dest='sensors', action='store_false')
+parser.set_defaults(sensors=False)
+
+# Add argument to specify whether or not to scan
+parser.add_argument('--scan', dest='scan', action='store_true')
+parser.add_argument('--no-scan', dest='scan', action='store_false')
+parser.set_defaults(scan=True)
 
 # Parse the args
 args = parser.parse_args()
+print(f'Sensors arg reads {args.sensors}')
 
 # First check to see what scan we are on - if we don't have
 # a scan number logged, assume it is the first scan and set
 # scan number to one. 
 
-if os.path.exists(config.SCAN_COUNT):
+if os.path.exists(config.SCAN_COUNT) == True:
 
     # Read scan count from file
     with open(config.SCAN_COUNT, 'r') as f:
@@ -56,13 +60,24 @@ else:
     # Scan count file does not exist - set scan count to 1
     scan_count = 1
 
-    # Since this is the first scan of the run, make the output
-    # directory to hold the scans
-    os.mkdir(config.SCAN_DIR)
+    # Since this is the first scan of the run, we may need to 
+    # make the output directory to hold the scans - if the 
+    # directory exist, it's OK, continute. Means that someone 
+    # probably deleted the scan count file by hand
 
-    # Also, create the sensor data file and write the header
-    with open(config.SENSOR_DATA, 'w') as f:
-        f.write(f'scan_number,datetime,pressure_hPa,temp_C,rel_humidity\n')
+    if os.path.exists(config.SCAN_DIR) == False:
+        os.mkdir(config.SCAN_DIR)
+
+    # Also, if we are running the sensors, create the sensor data 
+    # file and write the header
+
+    if args.sensors == True:
+        with open(config.SENSOR_DATA, 'w') as f:
+            f.write(f'scan_number,datetime,pressure_hPa,temp_C,rel_humidity\n')
+
+
+# If we are running the sensors, collect the data
+print(f'Done with pre-prep, checking sensors: {args.sensors}')
 
 if args.sensors == True:
     # Before doing the scan, read sensor data - the scan takes about
@@ -72,37 +87,43 @@ if args.sensors == True:
     # previous 10 minutes since the last scan.
 
     # Read sensors
+    print(f'Reading sensors')
     i2c = busio.I2C(board.SCL, board.SDA)
     sensor = MS8607(i2c)
 
     # Get current time
     scantime = datetime.now()
 
-    # Write data to file
+    # Write sensor data to file
     with open(config.SENSOR_DATA, 'a') as f:
         f.write(f'{scan_count},{scantime},{sensor.pressure},{sensor.temperature},{sensor.relative_humidity}\n')
 
-# Set up zero padded file name for new scan
-file_name = f'{scan_count:04}.{config.SCAN_FORMAT}'
-output = f'{config.SCAN_DIR}/{file_name}'
+# If we want to acquire a scan
+print(f'Acquiring scan: {args.scan}')
 
-# Construct scan command using parameters from config.py
-scan_command = " ".join([
-    'scanimage',
-    f'--format={config.SCAN_FORMAT}',
-    f'--resolution={config.RESOLUTION}',
-    f'--mode={config.SCAN_MODE}',
-    f'>{output}'
-])
+if args.scan == True:
 
-# Do the scan
-os.system(scan_command)
+    # Set up zero padded file name for new scan
+    file_name = f'{scan_count:04}.{config.SCAN_FORMAT}'
+    output = f'{config.SCAN_DIR}/{file_name}'
 
-# Rotate image 90 degrees to landscape if desired
-if config.LANDSCAPE == True:
-    img = Image.open(output)
-    img = img.rotate(-90, expand=1)
-    img.save(output)
+    # Construct scan command using parameters from config.py
+    scan_command = " ".join([
+        'scanimage',
+        f'--format={config.SCAN_FORMAT}',
+        f'--resolution={config.RESOLUTION}',
+        f'--mode={config.SCAN_MODE}',
+        f'>{output}'
+    ])
+
+    # Do the scan
+    os.system(scan_command)
+
+    # Rotate image 90 degrees to landscape if desired
+    if config.LANDSCAPE == True:
+        img = Image.open(output)
+        img = img.rotate(-90, expand=1)
+        img.save(output)
 
 # Write the updated scan count
 with open(config.SCAN_COUNT, 'w') as f:
